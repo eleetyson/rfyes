374a90ce605baa5c82fe7c1a7f36b8a4672f127a
Ethan <eleetyson@gmail.com>
2020-11-23 10:02:44 -0500

    set up redux store and thunk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore, applyMiddleware, compose } from 'redux'
+import ideasReducer from './reducers/ideasReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+
+const store = createStore(
+  ideasReducer,
+  compose(applyMiddleware(thunk), composeWithDevTools())
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
   document.getElementById('root')
 )

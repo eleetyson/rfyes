1b8ab2543ce72be4d81c780e4df39b171654c544
Ethan <eleetyson@gmail.com>
2020-11-23 15:10:54 -0500

    finish styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-light bg-white border-bottom">
-      <Link to="/" className="navbar-brand pl-xl-5 pl-lg-5 pl-md-5 pl-sm-2  ml-xl-3 ml-lg-3 ml-md-3">
+      <NavLink to="/" className="navbar-brand pl-xl-5 pl-lg-5 pl-md-5 pl-sm-2  ml-xl-3 ml-lg-3 ml-md-3">
         Logo here
-      </Link>
+      </NavLink>
 
       <div className="navbar-nav ml-auto flex-row">
-        <Link to="/all" className="nav-link px-3">Index</Link>
-        <Link to="/about" className="nav-link pr-xl-5 pr-lg-5 pr-md-5 pr-sm-2  mr-xl-5 mr-lg-5 mr-md-5  pl-xl-3 pl-lg-3 pl-md-3">About</Link>
+        <NavLink to="/" className="nav-link pr-3" activeClassName="nav-item active">Generator</NavLink>
+        <NavLink to="/all" className="nav-link px-3" activeClassName="nav-item active">Index</NavLink>
+        <NavLink to="/about" className="nav-link pr-xl-5 pr-lg-5 pr-md-5 pr-sm-2  mr-xl-5 mr-lg-5 mr-md-5  pl-xl-3 pl-lg-3 pl-md-3" activeClassName="nav-item active">About</NavLink>
       </div>
     </nav>
   )

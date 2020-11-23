34cc50b2d017050a1976eaa39df69ad35949f11e
Ethan <eleetyson@gmail.com>
2020-11-23 10:04:42 -0500

    comment index file

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import thunk from 'redux-thunk'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-
+// creating redux store using the ideas reducer
+// also connecting to redux dev tools
 const store = createStore(
   ideasReducer,
   compose(applyMiddleware(thunk), composeWithDevTools())
 )
 
+// wrap top-level App component so all child components can access redux store
+// wrap everything with router
 ReactDOM.render(
   <Router>
     <Provider store={store}>

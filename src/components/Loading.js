48376661529e6f76a7c4565853705f6d8e5bf5bc
Ethan <eleetyson@gmail.com>
2020-11-24 12:52:24 -0500

    adjust stylesheet, loading animation, and dynamic rendering for source link button

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,12 +5,10 @@ import home from '../animations/loading.json'
 const Loading = () => {
   return (
     <div className="card w-75 box-shadow mb-5">
-      <div className="card-body mx-xl-5 mx-lg-5 mx-md-2 mx-sm-1 mx-xs-1 py-xl-3 py-lg-3 my-xl-3 my-lg-3">
-        <LottieAnimation lotti={home} height={300} width={300} />
-      </div>
+      <LottieAnimation lotti={home} height={300} width={300} />
     </div>
   )
-
+  
 }
 
 export default Loading

6ea9b1d9d7729844de719f3a8b2374add2886216
Ethan <eleetyson@gmail.com>
2020-11-24 09:39:42 -0500

    add instruction and idea components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import { Switch, Route } from 'react-router-dom'
-import Generator from './components/Generator'
+import Generator from './containers/Generator'
 // import other components
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-
       <Switch>
-        <Route exact path="/">
-          <Generator />
-        </Route>
+        <Route exact path="/"> <Generator /> </Route>
       </Switch>
     </div>
   )
---
+++ src/components/Instruction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Instruction = () => {
+  return (
+    <div className="card w-75 bg-transparent border-0">
+      <div className="card-body">
+        <h5 className="text-right"><kbd>Enter</kbd> for new idea</h5>
+      </div>
+    </div>
+  )
+  
+}
+
+export default Instruction

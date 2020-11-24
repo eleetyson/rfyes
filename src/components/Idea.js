edeabceb5cb7c7e28e2428c0a101d671c6ce1089
Ethan <eleetyson@gmail.com>
2020-11-24 09:43:30 -0500

    build out idea component skeleton

---
+++ src/components/Idea.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Idea extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+
+}
+
+const mapDispatchToProps = dispatch => {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Idea)

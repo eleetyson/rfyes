da08f30a049e22f3973066b7f25e819872a0e350
Ethan <eleetyson@gmail.com>
2020-11-28 11:59:17 -0500

    fade in on initial page load and child route transitions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // top-level component always renders nav bar and and a component depending on route
 import './App.css'
 import Navbar from './components/Navbar'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import TransitionSwitch from 'react-router-transition-switch'
+import Fader from 'react-fader'
+
 import Generator from './containers/Generator'
 import IdeasIndex from './containers/IdeasIndex'
 import About from './containers/About'
@@ -11,12 +15,12 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Switch>
+      <TransitionSwitch component={Fader}>
         <Route exact path="/"> <Generator /> </Route>
         <Route exact path="/all"> <IdeasIndex /> </Route>
         <Route exact path="/about"> <About /> </Route>
         <Route exact path="/ideas/new"> <Form /> </Route>
-      </Switch>
+      </TransitionSwitch>
     </div>
   )
 
